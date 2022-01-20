0ffde418b22909d2d0ca01e7e513418d5c7ac24a
Tamara <80672923+gluschenkotoma@users.noreply.github.com>
2022-01-20 15:51:49 +0200

    button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { GlobalStyle } from './Components/GlobalStyles';
 import { SearchBar } from 'Components/SearchBar/SearchBar';
 import { ImageGallery } from 'Components/ImageGallery/ImageGallery';
+import { Button } from 'Components/Button/Button';
 function App() {
   return (
     <>
       <GlobalStyle />
       <SearchBar />
       <ImageGallery />
+      <Button />
     </>
   );
 }
