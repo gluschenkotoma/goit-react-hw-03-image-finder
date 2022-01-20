3d3fb19587142568d2c5a8003f64136913a67a1e
Tamara <80672923+gluschenkotoma@users.noreply.github.com>
2022-01-20 16:47:43 +0200

    modal

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { SearchBar } from 'Components/SearchBar/SearchBar';
 import { ImageGallery } from 'Components/ImageGallery/ImageGallery';
 import { Button } from 'Components/Button/Button';
 import { Loader } from 'Components/Loader/Loader';
+import { Modal } from 'Components/Modal/Modal';
+
 function App() {
   return (
     <>
       <GlobalStyle />
       <SearchBar />
       <ImageGallery />
+      <Modal />
       <Button />
       <Loader />
     </>
---
+++ src/Components/Modal/Modal.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react/cjs/react.development';
+
+export class Modal extends Component {
+  render() {
+    return (
+      <div class="overlay">
+        <div class="modal">
+          <img src="" alt="" />
+        </div>
+      </div>
+    );
+  }
+}
